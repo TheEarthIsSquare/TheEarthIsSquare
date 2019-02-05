cc09c05712e5ffb38ece31288aa2743bc4385fe5
Mitchell Romney <mitchellromney10@gmail.com>
2019-02-06 10:33:17 +1100

    SMALL: Added loading bar to loading screen.

--- website/static/js/all.js
+++ website/static/js/all.js
@@ -5,15 +5,12 @@ $(document).ready(function() {
   }, 4500);
 
   $('#opener').click(function() {
-    console.log('click has worked');
     $('.nav-container').toggleClass('open');
     $('.white-logo').toggleClass('open');
     if($('.opener i').hasClass('fa-bars')){
-      console.log('has bars');
       $('.opener i').removeClass('fa-bars');
       $('.opener i').addClass('fa-times');
     } else {
-      console.log('no bars');
       $('.opener i').removeClass('fa-times');
       $('.opener i').addClass('fa-bars');
     }
