12385f2a55a0f9a4770cb6fb0cc1036509f05d61
Mitchell Romney <mitchellromney10@gmail.com>
2019-02-06 16:04:56 +1100

    SMALL: Added Profile and Project database tables.

--- website/admin.py
+++ website/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+
+from website.models import Profile, Project, Image
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ('name', 'role')
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    list_display = ('name', 'client', 'date_completed')
+
+class ImageAdmin(admin.ModelAdmin):
+    model = Image
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Image, ImageAdmin)
---
+++ website/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='website/images/profiles')
+    twitter_username = models.CharField(max_length=255, null=True, blank=True)
+    instagram_username = models.CharField(max_length=255, null=True, blank=True)
+
+class Project(models.Model):
+    TYPE_CHOICES = (
+        (1, 'Website'),
+        (2, 'Design'),
+        (3, 'Marketing'),
+        (4, 'Social Media Campagin'),
+    )
+
+    name = models.CharField(max_length=255)
+    type = models.IntegerField(
+        choices=TYPE_CHOICES,
+        default=1,
+    )
+    client = models.CharField(max_length=255)
+    date_completed = models.DateField(default=datetime.now)
+    description = models.TextField(null=True, blank=True)
+    tesimonial = models.TextField(null=True, blank=True)
+    main_image = models.ImageField(upload_to='website/images/projects', null = True)
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='website/images/projects', null = True)
+    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
