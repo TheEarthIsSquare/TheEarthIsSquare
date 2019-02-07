c46ab016efacde8e7889cf29c33b719cabba520c
Mitchell Romney <mitchellromney10@gmail.com>
2019-02-07 13:13:08 +1100

    FIX: Wrong bucket setting name for Heroku.

--- theearthissquare/settings_production.py
+++ theearthissquare/settings_production.py
@@ -21,7 +21,7 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 
 S3DIRECT_ENDPOINT = 's3.amazonaws.com'  # http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
 S3DIRECT_DIR = 's3direct'  # (optional, default is 's3direct', location within the bucket to upload files)
