8d4c9e1aa6b0c63c6a7ccf38f1b1230f1ba782c9
Mitchell Romney <mitchellromney10@gmail.com>
2019-02-07 13:18:16 +1100

    FIX: Still working on AWS settings.

--- theearthissquare/settings_production.py
+++ theearthissquare/settings_production.py
@@ -23,6 +23,10 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 
+AWS_S3_REGION_NAME = 'ap-southeast-2'
+
+AWS_S3_ENDPOINT_URL = 'https://ap-southeast-2.amazonaws.com'
+
 S3DIRECT_ENDPOINT = 's3.amazonaws.com'  # http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
 S3DIRECT_DIR = 's3direct'  # (optional, default is 's3direct', location within the bucket to upload files)
 S3DIRECT_UNIQUE_RENAME = False # (optional, default is 'False', gives the uploaded file a unique filename)
