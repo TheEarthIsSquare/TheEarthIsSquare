07dc1dbf1eb6a23ac3ca623dc2a01ca6871e6ecb
Mitchell Romney <mitchellromney10@gmail.com>
2019-02-11 14:58:11 +1100

    FIX: Sending emails from contact and not mitchell.

--- website/views.py
+++ website/views.py
@@ -71,7 +71,7 @@ def contact(request):
             send_mail(
                 'New Contact Form',
                 content + ' REPLY TO: ' + contact_email,
-                settings.EMAIL_HOST_USER,
+                'contact@theearthissquare.com',
                 ['hello@theearthissquare.com'],
                 fail_silently=False,
             )
