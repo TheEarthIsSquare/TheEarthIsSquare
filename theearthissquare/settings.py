e4313735ca9cdbcb38d85b6a81dc97b44b2338e8
Mitchell Romney <mitchellromney10@gmail.com>
2019-02-22 12:04:52 +1100

    FIX: Celery worker fix 2.

--- theearthissquare/settings.py
+++ theearthissquare/settings.py
@@ -128,8 +128,7 @@ FACEBOOK_GRAPH_API_ACCESS_KEY = os.environ.get('FACEBOOK_GRAPH_API_ACCESS_KEY', 
 
 # Celery application definition
 # http://docs.celeryproject.org/en/v4.0.2/userguide/configuration.html
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
