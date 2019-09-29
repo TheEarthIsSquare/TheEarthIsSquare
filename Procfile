f80204b3f2a30a2a2b5ea9579fd8a10068a412ff
Mitchell Romney <mitchell@theearthissquare.com>
2019-09-29 12:55:39 +1000

    WEBSITE 2.0 -  VueJS, Construction Page.

--- Procfile
+++
@@ -1,3 +0,0 @@
-release: python manage.py migrate
-worker: celery -A theearthissquare worker -l info -B
-web: waitress-serve --port=$PORT theearthissquare.wsgi:application
--- admin_panel/templatetags/test.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.admin.sites import *
-from django import template
-
-register = template.Library()
-@register.simple_tag
-def all_apps ():
-    return self.get_app_list(request)
