453b93c488991d8aaf7d2fce23181651e4a9ad40
Mitchell Romney <mitchell@theearthissquare.com>
2022-11-09 12:28:17 +1100

    add page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-16",
+      title: "The Earth Is Square",
+      meta: [
+        // { name: "description", content: "My amazing site." }
+      ],
+    }
+  },
   css: [
     "@/assets/style/main.scss"
   ],
