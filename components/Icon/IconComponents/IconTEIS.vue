4217d3a406f2b957745353f3e79bc9ded5c1a937
Mitchell Romney <mitchell@theearthissquare.com>
2022-12-21 19:33:21 +1100

    update teis svg icon

--- components/Icon/IconComponents/IconTEIS.vue
+++ components/Icon/IconComponents/IconTEIS.vue
@@ -1,18 +1,24 @@
 <template>
-  <svg fill="none" height="34" viewBox="0 0 34 34" width="34" xmlns="http://www.w3.org/2000/svg">
-    <path d="M33.0874 0.912598H4.12988V14.8551H18.6086V29.8701H33.0874V0.912598Z" fill="url(#paint0_linear_1074_275)" />
-    <rect fill="transparent" height="12.1275" stroke="url(#paint1_linear_1074_275)" stroke-width="1.815" width="12.1275"
-          x="1.8201" y="20.0525" />
+  <svg fill="none" height="30" viewBox="0 0 30 30" width="30" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M28 0H5C3.89543 0 3 0.89543 3 2V11C3 12.1046 3.89543 13 5 13H14.5C15.6046 13 16.5 13.8954 16.5 15V25C16.5 26.1046 17.3954 27 18.5 27H28C29.1046 27 30 26.1046 30 25V2C30 0.895431 29.1046 0 28 0Z"
+        fill="url(#paint0_linear_595_125)"/>
+    <mask id="path-3-inside-1_595_125" fill="white">
+      <rect height="13" rx="2" width="13" y="17"/>
+    </mask>
+    <rect fill="white" height="13" mask="url(#path-3-inside-1_595_125)" rx="2" stroke="url(#paint1_linear_595_125)"
+          stroke-width="6" width="13"
+          y="17"/>
     <defs>
-      <linearGradient id="paint0_linear_1074_275" gradientUnits="userSpaceOnUse" x1="31.0262" x2="1.67965" y1="3.06432"
-                      y2="15.349">
-        <stop stop-color="#EB2188" />
-        <stop offset="1" stop-color="#D909DD" />
+      <linearGradient id="paint0_linear_595_125" gradientUnits="userSpaceOnUse" x1="28.0781" x2="0.715404" y1="2.00627"
+                      y2="13.4605">
+        <stop stop-color="#EB2188"/>
+        <stop offset="1" stop-color="#D909DD"/>
       </linearGradient>
-      <linearGradient id="paint1_linear_1074_275" gradientUnits="userSpaceOnUse" x1="13.8627" x2="-0.267143" y1="20.181"
-                      y2="26.0959">
-        <stop stop-color="#EB2188" />
-        <stop offset="1" stop-color="#D909DD" />
+      <linearGradient id="paint1_linear_595_125" gradientUnits="userSpaceOnUse" x1="12.0747" x2="-1.09999" y1="17.966"
+                      y2="23.481">
+        <stop stop-color="#EB2188"/>
+        <stop offset="1" stop-color="#D909DD"/>
       </linearGradient>
     </defs>
   </svg>
