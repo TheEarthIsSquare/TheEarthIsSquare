aedc5cc29e42c3d59ec011378285501d922aeed6
Mitchell Romney <mitchell@theearthissquare.com>
2023-01-08 14:17:58 +1100

    add sitemap.xml and robots.txt

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,4 +1,4 @@
-import type { RouterConfig } from "@nuxt/schema";
+import type {RouterConfig} from "@nuxt/schema";
 import Home from "~/pages/home/index.vue";
 import Course from "~/pages/course/index.vue";
 import Apply from "~/pages/apply.vue";
@@ -9,14 +9,14 @@ import Privacy from "~/pages/privacy.vue";
 import TOC from "~/pages/toc.vue";
 
 export default <RouterConfig>{
-  routes: (_routes) => [
-    { name: "home", path: "/", component: Home },
-    { name: "course", path: "/course", component: Course },
-    { name: "apply", path: "/apply", component: Apply },
-    { name: "contact", path: "/contact", component: Contact },
-    { name: "imprint", path: "/imprint", component: Imprint },
-    { name: "info", path: "/info", component: Info },
-    { name: "privacy", path: "/privacy", component: Privacy },
-    { name: "toc", path: "/toc", component: TOC },
-  ]
+    routes: (_routes) => [
+        { name: "home", path: "/", component: Home },
+        { name: "course", path: "/course", component: Course },
+        { name: "apply", path: "/apply", component: Apply },
+        { name: "contact", path: "/contact", component: Contact },
+        { name: "imprint", path: "/imprint", component: Imprint },
+        { name: "info", path: "/info", component: Info },
+        { name: "privacy", path: "/privacy", component: Privacy },
+        { name: "toc", path: "/toc", component: TOC },
+    ]
 };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,23 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      charset: "utf-16",
-      meta: [
-        // { name: "description", content: "My amazing site." }
-      ],
+    modules: [
+        '@funken-studio/sitemap-nuxt-3'
+    ],
+    css: [
+        "@/assets/style/main.scss"
+    ],
+    // @ts-ignore
+    sitemap: {
+        hostname: 'https://theearthissquare.com',
+        gzip: true,
+        filter: ({ routes }: { routes: any }) => {
+            return routes.filter((route: any) => !route.name.includes('features')).map((route: any) => {
+                if (route.name === 'home') {
+                    route.url = '/';
+                }
+
+                return route;
+            });
+        },
     }
-  },
-  css: [
-    "@/assets/style/main.scss"
-  ],
 });
